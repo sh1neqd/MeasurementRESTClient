3d4255ce3d47aab726fa7235fad171997657ec4a
sh1neqd <barton.g.bogdan@gmail.com>
2023-05-10 10:37:32 +0300

    Initial commit

---
+++ src/main/java/Client.java
@@ -0,0 +1,64 @@
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Client {
+    public static void main(String[] args) {
+        final String sensorName = "sensor1";
+
+//        registerSensor(sensorName);
+
+        Random random = new Random();
+
+        double maxTemperature = 30.0;
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+            sendMeasurement(random.nextDouble() * maxTemperature,
+                    true, sensorName);
+        }
+    }
+
+    private static void registerSensor(String sensorName) {
+        final String url = "http://localhost:3030/sensors/registration";
+
+        Map<String, Object> jsonData = new HashMap<>();
+        jsonData.put("name", sensorName);
+
+        makePostRequestWithJSONData(url, jsonData);
+    }
+
+    private static void sendMeasurement(double value, boolean raining, String sensorName) {
+        final String url = "http://localhost:3030/measurement/add";
+
+        Map<String, Object> jsonData = new HashMap<>();
+        jsonData.put("value", value);
+        jsonData.put("raining", raining);
+        jsonData.put("sensor", Map.of("name", sensorName));
+
+        makePostRequestWithJSONData(url, jsonData);
+    }
+
+    private static void makePostRequestWithJSONData(String url, Map<String, Object> jsonData) {
+        final RestTemplate restTemplate = new RestTemplate();
+
+        final HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Object> request = new HttpEntity<>(jsonData, headers);
+
+        try {
+            restTemplate.postForObject(url, request, String.class);
+
+            System.out.println("ok");
+        } catch (HttpClientErrorException e) {
+            System.out.println("error");
+            System.out.println(e.getMessage());
+        }
+    }
+}
